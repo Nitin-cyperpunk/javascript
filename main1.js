51b24637d832b9c817c54f6cd80691f3a8339cf6
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2024-11-03 23:55:00 +0530

    second commit

---
+++ main1.js
@@ -0,0 +1,10 @@
+let firstName = "Nitin";
+let age = 19;
+let havingGf = false;
+
+if (havingGf == true) {
+    console.log(firstName = " is commited")
+}
+ else  {
+    console.log(firstName = " is single")
+ }
