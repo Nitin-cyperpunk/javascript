9c9ece2fc584b309b78253babf2d1fb0b2287b74
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2024-12-28 00:38:48 +0530

    first commit

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ let age = 19;
 let havinggf = false;
 
 console.log(`This person's name is ${firstName}, age is ${age}, and having a girlfriend is ${havinggf}.`);
+console.log("typeof firstName:", typeof firstName);
+console.log("typeof age:", typeof age);
+console.log("typeof havinggf:", typeof havinggf);
