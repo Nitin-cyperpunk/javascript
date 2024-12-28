2cf2840dd05f47d473cd27dcc79facd57d2cd3dd
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2024-12-28 23:31:53 +0530

    first commit

--- array/big.js
+++ array/big.js
@@ -1,9 +1,10 @@
 const numbers = [1, 2, 5, 9, 16, 58, 36, 89, 46, 75, 102, 99, 856, 623, 999];
- let maxNumber = numbers[0];
+let maxNumber = numbers[0];
 
- for (let i = 1; i < numbers.length; i++) {
-    if(numbers[i] > maxNumber) {
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
         maxNumber = numbers[i];
-        console.log("the biggest number is:", maxNumber);
     }
- }
+}
+
+console.log("The biggest number is:", maxNumber);
