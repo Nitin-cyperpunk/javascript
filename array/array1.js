29d341142b56faa65ce088f00ee941e78b894cc1
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-05-13 01:36:50 +0530

    first commit

---
+++ array/array1.js
@@ -0,0 +1,16 @@
+const arr = []
+arr.push(1)
+arr.push(2)
+console.log(arr)
+arr.push(65)
+arr.push(8)
+arr.push(1056)
+console.log(arr)
+console.log(arr.length)
+
+
+console.log(arr.sort((a, b) => a - b));
+
+console.log(arr.indexOf(1056))
+console.log(arr.includes(65))
+console.log("nitin" , arr)
